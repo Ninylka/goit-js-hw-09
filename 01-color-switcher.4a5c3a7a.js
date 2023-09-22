62d0dbea15e6ef0585d19635ef55ea69a585f4fd
Ninylka <131779765+Ninylka@users.noreply.github.com>
2023-09-22 14:19:22 +0000

    Deploying to gh-pages from @ Ninylka/goit-js-hw-09@bd69aa1916e45bee8f4f1ddae6b06a068beee1e9 🚀

---
+++ 01-color-switcher.4a5c3a7a.js
@@ -0,0 +1,2 @@
+!function(){var t,e=document.querySelector("body"),n=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");n.addEventListener("click",(function(){t=setInterval((function(){e.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),n.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(t),a.disabled=!0,n.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.4a5c3a7a.js.map
